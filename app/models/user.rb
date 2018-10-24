961dadb3c40700a5700329751dd4ddcb58da5645
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-10-24 13:37:52 -0600

    adding factory bot

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :street_address, :email_address, :password, :role
-  
+
   has_many :orders
   has_many :locations
 end
