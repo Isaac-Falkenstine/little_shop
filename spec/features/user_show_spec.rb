ce535b1ceab762d9a1897fb254d8c4e640b878bf
Isaac Falkenstine <isaac@falkenstine.com>
2018-10-25 15:27:09 -0600

    Added functionaility for editing users info (WIP)

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -13,4 +13,36 @@ describe 'user show page' do
     expect(page).to have_content(user.city)
     expect(page).to have_content(user.email_address)
   end
+
+  it 'user can edit their info on their show page' do
+    visit '/'
+
+    click_on 'Register'
+
+    expect(current_path).to eq(new_user_path)
+    fill_in :user_email_address, with: "isaac@email.com"
+    fill_in :user_password, with: 'test1234'
+    fill_in :user_first_name, with: "Isaac"
+    fill_in :user_last_name, with: 'Falkenstine'
+    fill_in :user_street_address, with: "1234 Coral Ln."
+    fill_in :user_city, with: 'Denver'
+    fill_in :user_zip, with: 123456
+    fill_in :user_state, with: 'Colorado'
+
+    click_on 'Create User'
+    expect(current_path).to eq(user_path(User.last))
+
+    click_on("Edit your personal info")
+
+    expect(current_path).to eq(edit_user_path(User.last))
+
+    fill_in :user_first_name, with: "Isaak"
+    fill_in :user_last_name, with: 'Falkenstein'
+
+    click_on("Update User")
+    
+    expect(page).to have_content("Isaak")
+    expect(page).to have_content("Falkenstein")
+
+  end
 end
