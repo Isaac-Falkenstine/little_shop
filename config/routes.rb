80c18d67952d1da7edd8a82fe61e2e179b69f385
Emilio <thecraftedgem@tuta.io>
2018-10-25 15:06:12 -0600

    i think the logout works

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
   end
 
   resources :items
-  resources :users
-
+  resources :users, except: [:destroy]
 end
---
+++ spec/features/a_user_can_log_out_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature "a user can log out" do 
+  scenario "after when logged in" do 
+    user = create(:user)
+    visit '/'
+
+    click_on 'Register'
+
+    expect(current_path).to eq(new_user_path)
+    fill_in :user_email_address, with: "isaac@email.com"
+    fill_in :user_password, with: 'test1234'
+    fill_in :user_first_name, with: "Isaac"
+    fill_in :user_last_name, with: 'Falkenstine'
+    fill_in :user_street_address, with: "1234 Coral Ln."
+    fill_in :user_city, with: 'Denver'
+    fill_in :user_zip, with: 123456
+    fill_in :user_state, with: 'Colorado'
+    
+    click_on 'Create User'
+    expect(current_path).to eq(user_path(User.last))
+    expect(page).to have_content("Welcome #{User.last.first_name} to the Pubshop App!")
+    expect(page).to have_content("Welcome, #{User.last.first_name} #{User.last.last_name}")
+    save_and_open_page
+    click_on "Logout"
+    expect(current_path).to eq("/")
+    expect(page).to have_content("Login")
+  end
+end
