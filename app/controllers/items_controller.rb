d2f15db59cd0e9b2a940f231d2f05dc3c5325b93
Isaac Falkenstine <isaac@falkenstine.com>
2018-10-28 11:33:39 -0600

    Added testing for merchant items page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController
 
   def index
-    @items = Item.all
-    
+    if current_user == nil || current_user.role == 0
+      @items = Item.all
+    elsif current_user.role == 1
+      binding.pry
+      @items = current_user.items
+    end
   end
 end
