44e2bce7d54f183ea644df5713e5f65b35d92ea4
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-10-28 17:51:01 -0600

    working on adding items for merchant

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  
+
   def index
     @items = Item.all
   end
@@ -12,11 +12,11 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     if @item.save
       session[:item_id] = @item.id
-      flash[:success] = "Item #{@item.name} has been saved"
+      flash[:success] = "Item #{@item.name} has been created and saved"
       redirect_to dashboard_items_path
     else
       render :new
-      flash[:notice] = "failed! "
+      flash[:notice] = "failed! Item could not be created"
     end
   end
 
