cda313a800b174006425d07d0bc9aa93b814bea5
Isaac Falkenstine <isaac@falkenstine.com>
2018-10-28 16:13:27 -0600

    Merchants can edit their items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,23 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if Item.find(params[:id]).update(update_params)
+      redirect_to dashboard_items_path
+      flash[:notice] = "Info succesfully edited!"
+    else
+      redirect_to edit_item_path(params[:id])
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:item).permit(:name, :price, :thumbnail, :inventory, :description)
+  end
 end
