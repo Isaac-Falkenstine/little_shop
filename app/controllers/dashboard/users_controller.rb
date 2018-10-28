67a3d7cb81dbb01a619e04a07bb4863bf69cf515
normanrs <normanrs@live.com>
2018-10-28 12:14:27 -0600

    schema fixed

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -1,44 +1,7 @@
 class Dashboard::UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
 
   def show
     @user = User.find(params[:id])
   end
 
-  def create
-     @user = User.new(user_params)
-     if @user.save && (Math.log10(user_params[:zip].to_i).to_i + 1 == 5)
-       session[:user_id] = @user.id
-       flash[:success] = "Welcome #{@user.first_name} to the Pubshop App!"
-       redirect_to user_path(@user)
-     else
-       render :new
-     end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    if User.find(params[:id]).update(update_params)
-      redirect_to user_path(params[:id])
-      flash[:notice] = "Info succesfully edited!"
-    else
-      redirect_to edit_user_path(params[:id])
-      flash[:notice] = "That E-Mail is already in use!"
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :street_address, :city, :state, :zip, :email_address, :password)
-  end
-
-  def update_params
-    params.require(:user).permit(:first_name, :last_name, :street_address, :city, :state, :zip, :email_address)
-  end
 end
