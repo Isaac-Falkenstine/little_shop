74cf8adc966549be3fca5100129bce1b9e7824ac
Emilio <thecraftedgem@tuta.io>
2018-10-29 16:46:57 -0600

    carts works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :build_cart
+
 
   helper_method :current_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
+  
+  
   helper_method :current_user
+
+  def build_cart 
+    @cart ||= Cart.new(session[:cart])
+  end
 end
---
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'basic cart function' do
+  it 'can add itemns to cart' do
+    item_1 = create(:item)
+    item_2 = create(:item)
+    
+    visit items_path
+    
+    expect(page).to have_content('Cart Items: 0')
+    within "#item-#{item_1.id}" do
+      click_on "Add To Cart"
+    end
+   save_and_open_page
+    expect(page).to have_content('Cart Items: 1')
+   
+    within "#item-#{item_1.id}" do
+      click_on "Add To Cart"
+
+    end
+    expect(page).to have_content('Cart Items: 2')
+  end
+end
