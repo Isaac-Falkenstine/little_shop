2e5af9e3601121d25ed4de6e87a330a0164905b9
normanrs <normanrs@live.com>
2018-10-28 19:30:19 -0600

    admin can enable user

--- spec/features/admin_can_toggle_user_access_spec.rb
+++ spec/features/admin_can_toggle_user_access_spec.rb
@@ -7,6 +7,9 @@ describe "when youre a merchant or admin" do
 
     @default_user = User.create(first_name: "Dude", last_name: "User", street_address: "1234 Anywhere St.",
                            city: "Lone Tree", state: "Colorado", zip: 81234, email_address: "user@email.com", password: "pass123")
+
+    @disabled_user = User.create(first_name: "Sad", last_name: "User", street_address: "1234 Anywhere St.", enabled: false,
+                           city: "Lone Tree", state: "Colorado", zip: 81234, email_address: "saduser@email.com", password: "pass123")
   end
 
   it "disables a user" do
@@ -28,6 +31,28 @@ describe "when youre a merchant or admin" do
     click_on 'Login To The Pub'
     expect(page).to have_content("Account disabled")
     expect(page).to have_content("Login")
+
+  end
+
+  it "enables a user" do
+
+    visit '/'
+    click_on 'Login'
+    fill_in :email_address, with: @admin_user.email_address
+    fill_in :password, with: @admin_user.password
+    click_on 'Login To The Pub'
+    click_on "Account"
+    click_on "Users"
+    within "#user-#{@disabled_user.id}" do
+      click_on "Enable"
+    end
+    click_on 'Logout'
+    click_on 'Login'
+    fill_in :email_address, with: @disabled_user.email_address
+    fill_in :password, with: @disabled_user.password
+    click_on 'Login To The Pub'
+    expect(page).to have_content("Welcome")
+    expect(page).to have_content("Logout")
 
   end
 
