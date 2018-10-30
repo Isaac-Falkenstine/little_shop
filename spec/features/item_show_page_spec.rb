f8885da8846f4c4e7f8af3404237a85b9e215b52
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-10-30 02:52:34 -0600

    still need to figure out how to get merchant name and fix broken test

--- spec/features/item_show_page_spec.rb
+++ spec/features/item_show_page_spec.rb
@@ -7,7 +7,18 @@ describe 'user sees can go into an item show page' do
     visit item_path(item_1)
     expect(current_path).to eq(item_path(item_1))
     expect(page).to have_content(item_1.name)
-    expect(page).to have_content(item_1.description)
-    # expect(page).to have_content("Inventory: #{item_1.inventory}")
+    expect(page).to have_content("Description: #{item_1.description}")
+    expect(page).to have_content("Inventory: #{item_1.inventory}")
+    # expect(page).to have_content("Merchant Name: #{item_1}")
+  end
+
+  it 'can add item to cart' do
+    item_1 = create(:item)
+
+    visit items_path(item_1)
+
+    expect(page).to have_content('Cart Items: 0')
+    click_on "Add To Cart"
+    expect(page).to have_content('Cart Items: 1')
   end
 end
