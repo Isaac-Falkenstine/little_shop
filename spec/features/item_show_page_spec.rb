9e912d02f17b208f6b76b5aa0499b3cedc278163
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-10-30 09:55:47 -0600

    working item show page

---
+++ spec/features/item_show_page_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'user sees can go into an item show page' do
+  it 'can navigate to item show page' do
+    merchant_1 = create(:merchant)
+    item_1 = create(:item, user_id: merchant_1.id)
+
+    visit item_path(item_1)
+
+    expect(current_path).to eq(item_path(item_1))
+    expect(page).to have_content(item_1.name)
+
+    expect(page).to have_content("Description: #{item_1.description}")
+    expect(page).to have_content("Inventory: #{item_1.inventory}")
+  end
+
+  it 'can add item to cart' do
+    item_1 = create(:item)
+
+    visit items_path(item_1)
+
+    expect(page).to have_content('Cart Items: 0')
+    click_on "Add To Cart"
+    expect(page).to have_content('Cart Items: 1')
+  end
+end
