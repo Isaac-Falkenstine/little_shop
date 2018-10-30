b61956ca532423c6fb613ced74aacf221217565e
Isaac Falkenstine <isaac@falkenstine.com>
2018-10-29 19:49:17 -0600

    Made test pass

--- spec/features/user_can_see_their_cart_items_spec.rb
+++ spec/features/user_can_see_their_cart_items_spec.rb
@@ -29,13 +29,10 @@ RSpec.describe 'basic cart function' do
     expect(current_path).to eq("/cart")
 
     expect(page).to have_content("#{item_1.name}")
-    expect(page).to have_content("#{item_1.price}")
+    expect(page).to have_content("Item price: $#{item_1.price}")
     expect(page).to have_content("#{merchant_1.first_name} #{merchant_1.last_name}")
-    expect(page).to have_content("1") #Price for a item
-    expect(page).to have_content("Total price for item: 3") #Total price for a item
-    expect(page).to have_content("Amount in cart: 3") #Amount of a item in cart
-    expect(page).to have_content("Total Cost for Cart: 5") #Total for everyhting in cart
-
-    # save_and_open_page
+    expect(page).to have_content("Total price for item: $3")
+    expect(page).to have_content("Amount in cart: 3")
+    expect(page).to have_content("Total Cost for Cart: $5")
   end
 end
