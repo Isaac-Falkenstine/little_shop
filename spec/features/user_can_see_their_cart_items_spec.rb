a8d7110aefd4c9b19fda1ce4d2ea808baf9e9699
normanrs <normanrs@live.com>
2018-10-31 13:37:44 -0600

    passing tests

--- spec/features/user_can_see_their_cart_items_spec.rb
+++ spec/features/user_can_see_their_cart_items_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'basic cart function' do
-  it 'can add items to cart and see those items' do
+  it 'visitor can add and view cart items' do
     merchant_1 = create(:merchant)
     item_1 = create(:item, user_id: merchant_1.id, inventory: 80)
     item_2 = create(:item, user_id: merchant_1.id, inventory: 80)
@@ -36,7 +36,7 @@ RSpec.describe 'basic cart function' do
     expect(page).to have_content("Total Cost for Cart: $5")
   end
 
-  it 'can empty cart' do
+  it 'visitor can empty cart' do
     merchant_1 = create(:merchant)
     item_1 = create(:item, user_id: merchant_1.id, inventory: 80)
     item_2 = create(:item, user_id: merchant_1.id, inventory: 80)
@@ -70,7 +70,7 @@ RSpec.describe 'basic cart function' do
     expect(page).to have_content("Cart Items: 0")
   end
 
-  it 'can add items from cart' do
+  it 'visitor can add items from cart' do
     merchant_1 = create(:merchant)
     item_1 = create(:item, user_id: merchant_1.id, inventory: 80)
 
@@ -87,7 +87,7 @@ RSpec.describe 'basic cart function' do
     expect(current_path).to eq(cart_path)
   end
 
-  it 'can subtract items from cart' do
+  it 'visitor can subtract items from cart' do
     merchant_1 = create(:merchant)
     item_1 = create(:item, user_id: merchant_1.id, inventory: 80)
 
@@ -103,7 +103,7 @@ RSpec.describe 'basic cart function' do
     expect(current_path).to eq(cart_path)
   end
 
-  it 'can subtract items from cart with restrictions' do
+  it 'visitor can subtract items from cart with restrictions' do
     merchant_1 = create(:merchant)
     item_1 = create(:item, user_id: merchant_1.id, inventory: 30)
     item_2 = merchant_1.items.create(name: "Glove", description: "You can catch stuff with it!", price: 20.00, inventory: 1, thumbnail: "img.jpeg")
@@ -129,7 +129,7 @@ RSpec.describe 'basic cart function' do
     expect(current_path).to eq(cart_path)
   end
 
-  it 'can add items from cart with restrictions' do
+  it 'visitor can add items from cart with restrictions' do
     merchant_1 = create(:merchant)
     item_1 = create(:item, user_id: merchant_1.id, inventory: 30)
     item_2 = merchant_1.items.create(name: "Glove", description: "You can catch stuff with it!", price: 20.00, inventory: 1, thumbnail: "img.jpeg")
@@ -155,4 +155,30 @@ RSpec.describe 'basic cart function' do
 
     expect(current_path).to eq(cart_path)
   end
+
+  describe 'visitory cannot place an order' do
+    it 'notifies they must register or login and redirects to registration' do
+      merchant_1 = create(:merchant)
+      item_1 = create(:item, user_id: merchant_1.id, inventory: 30)
+      item_2 = merchant_1.items.create(name: "Glove", description: "You can catch stuff with it!", price: 20.00, inventory: 1, thumbnail: "img.jpeg")
+
+      visit items_path
+
+      within "#item-#{item_1.id}" do
+        click_on "Add To Cart"
+      end
+
+      within "#item-#{item_2.id}" do
+        click_on "Add To Cart"
+      end
+
+      click_on "Cart Items"
+      click_on "Check Out"
+
+      expect(page).to have_content("You must register or login to checkout.")
+      expect(current_path).to eq(new_user_path)
+    end
+  end
+
+
 end
