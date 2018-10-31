d100c0903493a2acdd87513a76bd18af600ef617
Isaac Falkenstine <isaac@falkenstine.com>
2018-10-30 19:22:26 -0600

    When logged out your cart empties

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,7 +21,8 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    # session.clear to clear the cart maybe?
+
+    @cart.contents.clear
     flash[:notice] = "You have been logged out"
     redirect_to '/'
   end
